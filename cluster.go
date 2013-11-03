b43552bfa7d7f1b733e1eb5e0da502f2b98e8111
Will Farrington <wfarr@github.com>
2013-11-02 19:04:29 -0700

    go fmt

--- cluster.go
+++ cluster.go
@@ -1,80 +1,80 @@
 package main
 
 import (
-  "encoding/json"
-  "io/ioutil"
-  "net/http"
-  "fmt"
-  "os"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
 )
 
 type Cluster struct {
-  Ip string
-  Port string
+	Ip   string
+	Port string
 }
 
 type ClusterHealth struct {
-  Status              string `json:"status"`
-  ClusterName         string `json:"cluster_name"`
-  TimedOut            bool   `json:"timed_out"`
-  NumberOfNodes       int    `json:"number_of_nodes"`
-  NumberOfDataNodes   int    `json:"number_of_data_nodes"`
-  ActivePrimaryShards int    `json:"active_primary_shards"`
-  ActiveShards        int    `json:"active_shards"`
-  RelocatingShards    int    `json:"relocating_shards"`
-  InitializingShards  int    `json:"initializing_shards"`
-  UnassignedShards    int    `json:"unassigned_shards"`
+	Status              string `json:"status"`
+	ClusterName         string `json:"cluster_name"`
+	TimedOut            bool   `json:"timed_out"`
+	NumberOfNodes       int    `json:"number_of_nodes"`
+	NumberOfDataNodes   int    `json:"number_of_data_nodes"`
+	ActivePrimaryShards int    `json:"active_primary_shards"`
+	ActiveShards        int    `json:"active_shards"`
+	RelocatingShards    int    `json:"relocating_shards"`
+	InitializingShards  int    `json:"initializing_shards"`
+	UnassignedShards    int    `json:"unassigned_shards"`
 }
 
 type ClusterState struct {
-  ClusterName string `json:"cluster_name"`
-  MasterNode  string `json:"master_node"`
+	ClusterName string `json:"cluster_name"`
+	MasterNode  string `json:"master_node"`
 }
 
 type ClusterSettings struct {
-  Persistent SettingsYo `json:"persistent"`
-  Transient  SettingsYo `json:"transient"`
+	Persistent SettingsYo `json:"persistent"`
+	Transient  SettingsYo `json:"transient"`
 }
 
 type SettingsYo struct {
-  AllocationDisabled bool `json:"allocation_disabled"`
+	AllocationDisabled bool `json:"allocation_disabled"`
 }
 
 func (c *Cluster) GetHealth() (data ClusterHealth) {
-  get("/_cluster/health", &data)
-  return
+	get("/_cluster/health", &data)
+	return
 }
 
 func (c *Cluster) GetState() (data ClusterState) {
-  get("/_cluster/state", &data)
-  return
+	get("/_cluster/state", &data)
+	return
 }
 
 func (c *Cluster) GetSettings() (data ClusterSettings) {
-  get("/_cluster/settings", &data)
-  return
+	get("/_cluster/settings", &data)
+	return
 }
 
-func get(path string, buf interface{}) (interface{}) {
-  resp, err := http.Get("http://" + ip + ":" + port + path)
+func get(path string, buf interface{}) interface{} {
+	resp, err := http.Get("http://" + ip + ":" + port + path)
 
-  if err != nil || resp.StatusCode > 200 {
-    fmt.Printf("Could not find cluster at `%s:%s`!\n", ip, port)
-    os.Exit(1)
-  }
+	if err != nil || resp.StatusCode > 200 {
+		fmt.Printf("Could not find cluster at `%s:%s`!\n", ip, port)
+		os.Exit(1)
+	}
 
-  body, err := ioutil.ReadAll(resp.Body)
+	body, err := ioutil.ReadAll(resp.Body)
 
-  if err != nil {
-    panic(err.Error())
-  }
+	if err != nil {
+		panic(err.Error())
+	}
 
-  err = json.Unmarshal(body, &buf)
+	err = json.Unmarshal(body, &buf)
 
-  if err != nil {
-    fmt.Println("That's not JSON buddy!")
-    os.Exit(1)
-  }
+	if err != nil {
+		fmt.Println("That's not JSON buddy!")
+		os.Exit(1)
+	}
 
-  return buf
+	return buf
 }
--- main.go
+++ main.go
@@ -1,60 +1,60 @@
 package main
 
 import (
-  "flag"
-  "fmt"
-  "os"
+	"flag"
+	"fmt"
+	"os"
 )
 
 var ip string
 var port string
 
 func main() {
-  flag.StringVar(&ip, "ip", "127.0.0.1", "The host elasticsearch is running on")
-  flag.StringVar(&port, "port", "9200", "The port elasticsearch is running on")
+	flag.StringVar(&ip, "ip", "127.0.0.1", "The host elasticsearch is running on")
+	flag.StringVar(&port, "port", "9200", "The port elasticsearch is running on")
 
-  flag.Parse()
+	flag.Parse()
 
-  cluster := &Cluster{
-    Ip: ip,
-    Port: port,
-  }
+	cluster := &Cluster{
+		Ip:   ip,
+		Port: port,
+	}
 
-  switch flag.Arg(0) {
-    case "status":
-      health, _, settings := cluster.GetHealth(), cluster.GetState(), cluster.GetSettings()
-      var allocation string
+	switch flag.Arg(0) {
+	case "status":
+		health, _, settings := cluster.GetHealth(), cluster.GetState(), cluster.GetSettings()
+		var allocation string
 
-      if (settings.Persistent.AllocationDisabled || settings.Transient.AllocationDisabled) {
-        allocation = "disabled"
-      } else {
-        allocation = "enabled"
-      }
+		if settings.Persistent.AllocationDisabled || settings.Transient.AllocationDisabled {
+			allocation = "disabled"
+		} else {
+			allocation = "enabled"
+		}
 
-      fmt.Printf("Cluster:\n  Name: %s\n  State: %s\n  Allocation: %s\n  Relocating Shards: %v\n  Initializing Shards: %v\n  Unassigned Shards: %v\n",
-        health.ClusterName,
-        health.Status,
-        allocation,
-        health.RelocatingShards,
-        health.InitializingShards,
-        health.UnassignedShards)
-    default:
-      usage()
-      os.Exit(1)
-  }
+		fmt.Printf("Cluster:\n  Name: %s\n  State: %s\n  Allocation: %s\n  Relocating Shards: %v\n  Initializing Shards: %v\n  Unassigned Shards: %v\n",
+			health.ClusterName,
+			health.Status,
+			allocation,
+			health.RelocatingShards,
+			health.InitializingShards,
+			health.UnassignedShards)
+	default:
+		usage()
+		os.Exit(1)
+	}
 
-  os.Exit(0)
+	os.Exit(0)
 }
 
 func usage() {
-  if !flag.Parsed() {
-    flag.Parse()
-  }
+	if !flag.Parsed() {
+		flag.Parse()
+	}
 
-  fmt.Fprintf(os.Stderr, "Usage: %s [flags] [command] <[subcommand]>\n\n", os.Args[0])
-  fmt.Fprintf(os.Stderr, "FLAGS\n\n")
-  flag.PrintDefaults()
-  fmt.Fprintf(os.Stderr, "\n")
-  fmt.Fprintf(os.Stderr, "COMMANDS\n\n")
-  fmt.Fprintf(os.Stderr, "    status      display overall health\n\n")
+	fmt.Fprintf(os.Stderr, "Usage: %s [flags] [command] <[subcommand]>\n\n", os.Args[0])
+	fmt.Fprintf(os.Stderr, "FLAGS\n\n")
+	flag.PrintDefaults()
+	fmt.Fprintf(os.Stderr, "\n")
+	fmt.Fprintf(os.Stderr, "COMMANDS\n\n")
+	fmt.Fprintf(os.Stderr, "    status      display overall health\n\n")
 }
