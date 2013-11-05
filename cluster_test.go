4005903db475f8ef841b7fc55f361d0bfd97d9b0
Will Farrington <wfarr@github.com>
2013-11-05 04:00:24 -0800

    Start spiking out what tests will look like

---
+++ cluster_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"net/http/httptest"
+)
+
+import "testing"
+
+func SetupTestServer(handler func(w http.ResponseWriter, r *http.Request)) (ts *httptest.Server) {
+	ts = httptest.NewServer(http.HandlerFunc(handler))
+	return
+}
+
+
+func TestGetHealth(t *testing.T) {
+	ts := SetupTestServer(func(w http.ResponseWriter, r *http.Request) {
+		status := `{"status": "tangerine"}`
+
+		fmt.Fprintf(w, status)
+	})
+
+	defer ts.Close()
+
+	cluster := &Cluster{
+		URL:   ts.URL,
+	}
+
+	status := cluster.GetHealth().Status
+
+	if status != "tangerine" {
+		t.Log("Expected status to be `tangerine`, got", status)
+		t.Fail()
+	}
+}
