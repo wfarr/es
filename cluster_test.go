4e5d2f002f77e448dfa8337173f9519652360dba
Will Farrington <wfarr@github.com>
2013-11-05 04:15:46 -0800

    Add more comprehensive GetHealth() tests

--- cluster_test.go
+++ cluster_test.go
@@ -19,7 +19,15 @@ func MockServer(resp string) (ts *httptest.Server) {
 func TestGetHealth(t *testing.T) {
 	ts := MockServer(`{
 		"status": "tangerine",
-		"cluster_name": "foobar"
+		"cluster_name": "foobar",
+		"timed_out" : false,
+		"number_of_nodes" : 1,
+		"number_of_data_nodes" : 1,
+		"active_primary_shards" : 10,
+		"active_shards" : 20,
+		"relocating_shards" : 2,
+		"initializing_shards" : 0,
+		"unassigned_shards" : 0
 	}`)
 	defer ts.Close()
 
@@ -29,12 +37,33 @@ func TestGetHealth(t *testing.T) {
 	health := cluster.GetHealth()
 
 	if health.Status != "tangerine" {
-		t.Log("Expected status to be `tangerine`, got", health.Status)
 		t.Fail()
 	}
-
 	if health.ClusterName != "foobar" {
-		t.Log("Expected cluster name to be `foobar`, got", health.ClusterName)
+		t.Fail()
+	}
+	if health.TimedOut != false {
+		t.Fail()
+	}
+	if health.NumberOfNodes != 1 {
+		t.Fail()
+	}
+	if health.NumberOfDataNodes != 1 {
+		t.Fail()
+	}
+	if health.ActivePrimaryShards != 10 {
+		t.Fail()
+	}
+	if health.ActiveShards != 20 {
+		t.Fail()
+	}
+	if health.RelocatingShards != 2 {
+		t.Fail()
+	}
+	if health.InitializingShards != 0 {
+		t.Fail()
+	}
+	if health.UnassignedShards != 0 {
 		t.Fail()
 	}
 }
