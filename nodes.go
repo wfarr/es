e4869986e426b085f947c499a89e7b4f6be60a63
Will Farrington <wfarr@github.com>
2014-05-28 16:22:35 -0700

    Support 1.2.x node hostname

--- nodes.go
+++ nodes.go
@@ -25,7 +25,7 @@ func runNodes(c *Cluster, cmd *Command, args []string) error {
 	t := termtable.NewTable(&termtable.TableOptions{Padding: 1, Header: []string{"NAME", "HOSTNAME", "VERSION", "HTTP ADDRESS", "ATTRIBUTES"}})
 
 	for _, node := range nodes.Nodes {
-		t.AddRow([]string{node.Name, node.Hostname, node.Version, node.HTTPAddress, fmt.Sprintf("%v", node.Attributes)})
+		t.AddRow([]string{node.Name, node.Hostname + node.Host, node.Version, node.HTTPAddress, fmt.Sprintf("%v", node.Attributes)})
 	}
 
 	fmt.Println(t.Render())
